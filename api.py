4e93eb1737e8c5c8e187d0af834a1e6ed4513e00
Rhiannon99 <78912148+Rhiannon99@users.noreply.github.com>
2024-08-19 13:53:37 +0200

    Update api.py

--- api.py
+++ api.py
@@ -1,61 +1,36 @@
 from flask import Flask, jsonify, request
-from Media_Details_Extractors.watchseries import WatchSeriesExtractor
 from Media_Details_Extractors.vidsrc2 import Vidsrc
 
 app = Flask(__name__)
 
-@app.route('/VIDSRC-TO/Watch', methods=['GET'])
-def get_vidsrc_to_streams():
-    tmdb_id = request.args.get('tmdb_id')
-    media_type = request.args.get('type')
-    season = request.args.get('season')
-    episode = request.args.get('episode')
+# Route for fetching movie streams
+@app.route('/vidsrc/movie', methods=['GET'])
+def get_movie_streams():
+    tmdb_id = request.args.get('id')
+
+    if not tmdb_id:
+        return jsonify({"error": "Missing 'id' parameter"}), 400
 
     vidsrc = Vidsrc()
-    if media_type.lower() == 'tv':
-        m3u8_links = vidsrc.fetch_tv(tmdb_id,season,episode)
-    elif media_type.lower() == 'movie':
-        m3u8_links = vidsrc.fetch_movie(tmdb_id)
-    else:
-        m3u8_links = []
+    m3u8_links = vidsrc.fetch_movie(tmdb_id)
 
     return jsonify({"m3u8_links": m3u8_links, "subtitles": []})
 
-@app.route('/WATCHSERIES/Watch', methods=['GET'])
-def get_f2cloud_watchseries():
-    # Fetch Query Strings
-    media_id = request.args.get('id')
+# Route for fetching TV show streams
+@app.route('/vidsrc/tv', methods=['GET'])
+def get_tv_streams():
+    tmdb_id = request.args.get('id')
     season = request.args.get('season')
     episode = request.args.get('episode')
 
-    vse = WatchSeriesExtractor()
-    m3u8_links = vse.get_streams(media_id,season,episode)
+    if not tmdb_id or not season or not episode:
+        return jsonify({"error": "Missing 'id', 'season', or 'episode' parameter"}), 400
+
+    vidsrc = Vidsrc()
+    m3u8_links = vidsrc.fetch_tv(tmdb_id, season, episode)
+
     return jsonify({"m3u8_links": m3u8_links, "subtitles": []})
 
-@app.route('/WATCHSERIES/Search', methods=['GET'])
-def search():
-    # Fetch Query Strings
-    vs =  WatchSeriesExtractor()
-    query = request.args.get('query')
-    results = vs.fetch_search_results(query)
-    return jsonify(results)
-
-@app.route('/WATCHSERIES/Trending', methods=['GET'])
-def get_trending():
-    vs =  WatchSeriesExtractor()
-    results = vs.return_trending_json()
-    return jsonify(results)
-
-@app.route('/WATCHSERIES/Details', methods=['GET'])
-def get_media_details():
-    media_id = request.args.get('media_id')
-
-    if not media_id:
-        return jsonify("Need media_id")
-
-    vs =  WatchSeriesExtractor()
-    results = vs.fetch_media_details(media_id)
-    return jsonify(results)
-
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5002, debug=True)
+
