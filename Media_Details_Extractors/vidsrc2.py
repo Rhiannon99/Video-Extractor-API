5ab01ee9838c767858859e0274a4a1c360044df1
Rhiannon99 <78912148+Rhiannon99@users.noreply.github.com>
2024-08-19 13:55:28 +0200

    Update vidsrc2.py

--- Media_Details_Extractors/vidsrc2.py
+++ Media_Details_Extractors/vidsrc2.py
@@ -1,18 +1,11 @@
 import base64
 import re
-
 import cloudscraper
-from urllib.parse import quote
-
-import urllib.parse
-from bs4 import BeautifulSoup
-from urllib.parse import unquote
-from typing import Optional, Dict, List
-
+from urllib.parse import quote, unquote
+import time
 from utils import VidSrcError, mapp, rc4, reverse, subst, subst_
 from Video_Extractors.f2cloud import F2Cloud
 from keys import source_keys
-import time
 
 class Vidsrc:
     HOST = 'vidsrc2.to'
@@ -22,9 +15,7 @@ class Vidsrc:
 
     @staticmethod
     def enc(inp):
-        print(Vidsrc.keys)
         a = mapp(subst(rc4(Vidsrc.keys[0], reverse(inp))), Vidsrc.keys[1], Vidsrc.keys[2])
-        print(f"init a {a}")
         a = mapp(reverse(subst(rc4(Vidsrc.keys[3], a))), Vidsrc.keys[4], Vidsrc.keys[5])
         a = subst(rc4(Vidsrc.keys[8], reverse(mapp(a, Vidsrc.keys[6], Vidsrc.keys[7]))))
         return subst(a)
@@ -40,7 +31,6 @@ class Vidsrc:
     @staticmethod
     def fetch_streams_and_subtitles(data_id):
         time_millis = hex(int(time.time() * 1000))[2:]
-
         time_query_params = f"t={quote(time_millis)}&h={quote(Vidsrc.enc(time_millis))}"
         url = f"https://{Vidsrc.HOST}/ajax/embed/episode/{data_id}/sources?token={quote(Vidsrc.enc(data_id))}&{time_query_params}"
         req = Vidsrc.scraper.get(url)
@@ -48,21 +38,38 @@ class Vidsrc:
         if req.status_code != 200:
             raise VidSrcError(f"Couldnt fetch {req.url}, status code: {req.status_code}...")
 
-        f2_cloud_id = req.json()['result'][0]['id']
-        url = f"https://{Vidsrc.HOST}/ajax/embed/source/{f2_cloud_id}?token={quote(Vidsrc.enc(f2_cloud_id))}&{time_query_params}"
+        result = req.json().get('result', [])
+        if not result or len(result) < 2:
+            raise VidSrcError("Invalid response structure for episode sources")
+
+        f2_cloud_id = result[0]['id']
+        filemoon_id = result[1]['id']
+
+        # Fetching F2Cloud stream
+        f2cloud_url = Vidsrc.fetch_source_url(f2_cloud_id, time_query_params)
+        decrypted_f2cloud_url = Vidsrc.dec(f2cloud_url)
+
+        # Fetching Filemoon embed URL
+        filemoon_url = Vidsrc.fetch_source_url(filemoon_id, time_query_params)
+        decrypted_filemoon_url = Vidsrc.dec(filemoon_url)
+
+        return {
+            'f2cloud': F2Cloud().stream(decrypted_f2cloud_url, Vidsrc.source),
+            'filemoon_embed': decrypted_filemoon_url,  # Returning the Filemoon embed URL
+        }
+
+    @staticmethod
+    def fetch_source_url(source_id, time_query_params):
+        url = f"https://{Vidsrc.HOST}/ajax/embed/source/{source_id}?token={quote(Vidsrc.enc(source_id))}&{time_query_params}"
         req = Vidsrc.scraper.get(url)
 
         if req.status_code != 200:
             raise VidSrcError(f"Couldnt fetch {req.url}, status code: {req.status_code}...")
 
-        f2cloud_url = req.json()['result']['url']
-        decrypted_url = Vidsrc.dec(f2cloud_url)
-        return F2Cloud().stream(decrypted_url,Vidsrc.source)
+        return req.json().get('result', {}).get('url')
 
-
-    def fetch_movie(self,tmdb_id):
+    def fetch_movie(self, tmdb_id):
         url = f"https://{self.HOST}/embed/movie/{tmdb_id}"
-
         req = self.scraper.get(url)
         if req.status_code != 200:
             raise VidSrcError(f"Couldnt fetch {req.url}, status code: {req.status_code}...")
@@ -74,7 +81,7 @@ class Vidsrc:
 
         return self.fetch_streams_and_subtitles(data_id)
 
-    def fetch_tv(self,tmdb_id,season,episode):
+    def fetch_tv(self, tmdb_id, season, episode):
         url = f"https://{self.HOST}/embed/tv/{tmdb_id}/{season}/{episode}"
         req = self.scraper.get(url)
         if req.status_code != 200:
